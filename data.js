1acb19d24d9a9116adbe7fd9e36fb159eb7cc3b1
Archit Verma <architv07@gmail.com>
2015-02-12 23:12:55 +0530

    added cards for archived and pending

--- data.js
+++ data.js
@@ -16,6 +16,9 @@ function getChallengeData() {
 	    	// $("#cd-tabs").show();
 	    	document.getElementById("c-tabs").style.visibility = "visible";
 	    	generateCards(data);
+	    	generateCardsArchive(data);
+	    	generateCardsPending(data);
+	    		
 	    },
 	    error: function(jq, status, message) {
             // alert('A jQuery error has occurred. Status: ' + status + ' - Message: ' + message);
@@ -180,14 +183,261 @@ function generateCards(data) {
 	}
 }
 
+function generateCardsPending(data) {
+
+	var pending_tabs = document.getElementById("pending-contests");
+	pending_tabs.innerHTML = "";
+	console.log(JSON.parse(data));
+	// pending_tabs.innerText = JSON.parse(data)["pending"];
+
+	var pending_contest_data = JSON.parse(data)["pending"];
+
+	var randomNumber = 0;
+	hosts = JSON.parse(localStorage.getItem('hosts'));
+	
+	for (var i = 0; i < pending_contest_data.length; i++) {
+
+	
+		var newDiv = document.createElement('div');
+		newDiv.style.width = "360px";
+	 	newDiv.style.height = "130px"; 
+	 	newDiv.style.borderStyle = "solid"; 
+	 	newDiv.style.borderWidth = "2px";
+	 	newDiv.style.borderColor = "black";
+	 	newDiv.style.borderRadius = "8px 8px 8px 8px";
+	 	newDiv.style.borderColor = "black";
+	 	newDiv.style.marginBottom = "15px";
+	 
+
+	 	if(randomNumber<9){
+	 		randomNumber = randomNumber+1;
+		}else{
+			randomNumber = 1;
+		}
+
+
+	 	if(randomNumber==1){
+			newDiv.style.background = "#66FF33"; 
+	 	}else if(randomNumber==4){
+			newDiv.style.background = "#FFFF4D"; 
+	 	}else if(randomNumber==6){
+			newDiv.style.background = "#FF8533"; 
+	 	}else if(randomNumber==2){
+			newDiv.style.background = "#FF85AD"; 
+	 	}else if(randomNumber==8){
+			newDiv.style.background = "#DB94FF"; 
+	 	}else if(randomNumber==3){
+			newDiv.style.background = "#70B8FF"; 
+	 	}else if(randomNumber==5){
+			newDiv.style.background = "#82FFFF"; 
+	 	}else if(randomNumber==7){
+			newDiv.style.background = "#CCFF33"; 
+	 	}else if(randomNumber==9){
+			newDiv.style.background = "#47DAB5"; 
+	 	}
+
+		var startTime = toTimeZone(pending_contest_data[i].start).split(",");
+		var endTime = toTimeZone(pending_contest_data[i].end).split(",");
+
+		var lengthOfContestname = pending_contest_data[i].contest_name.length;
+		
+		if(imageExists("/img/"+pending_contest_data[i].host_name+".png")){
+			if(lengthOfContestname < 30) {
+
+	 		 	newDiv.innerHTML ="<img src='/img/"+ pending_contest_data[i].host_name  +".png' style='border:2px solid black; border-radius: 10px; margin-top:1px; margin-left:1px;height:30%;width30%;margin-bottom:12px;'>"+
+	 						"<span style='color:black; font-size:12px; font-family: Roboto, sans-serif;'>"+"  "+"<div style=' float:right; margin-top:5px; margin-right:3px'>"	+startTime[0] +"<br>"+
+	 					  "<i class='fa fa-play' style=' margin-right:5px;'></i>"+startTime[1] +"</div>" +"<br>"+ 
+	 					  "<span style='color:black; font-size:24px;  font-family: Courgette, cursive;'>"+"<div style='text-align:center; margin-top:5px; '>"+"<a href='"+pending_contest_data[i].contest_url +"' target='_blank' style='color:black'>" +pending_contest_data[i].contest_name+ "</a></div>" +
+	 					  "<span style='color:black; font-size:14px; font-family: Inconsolata, ;'>"+"<div style='text-align:center; margin-top:0px; '>"+"<a href='"+pending_contest_data[i].host_url +"' target='_blank' style='color:black'>" + pending_contest_data[i].host_name +"</a></div>" +"<br>"+
+	 					  "<span style='color:black; font-size:12px; font-family: Roboto, sans-serif;'>"+"  "+"<div style='float:right; margin-top:-20px; margin-right:3px; margin-bottom:3px'>"+ endTime[0] +"<br>"+  
+	 					  "<i class='fa fa-stop' style=' margin-right:5px'></i>"+ endTime[1] +"</div>"+ "<br>"+  
+	 					  "<span style='color:black; font-size:18px; margin-top:5px'>"+"<div style='text-align:left; margin-top:-35px'>"+"  "+ pending_contest_data[i].duration +"</span>";
+
+				
+	 	}else{
+				newDiv.innerHTML ="<img src='/img/"+ pending_contest_data[i].host_name +".png' style='border:2px solid black; border-radius: 10px; margin-top:1px; margin-left:1px;height:30%;width30%;margin-bottom:12px;'>"+
+	 					  "<span style='color:black; font-size:12px; font-family: Roboto, sans-serif;'>"+"  "+"<div style=' float:right; margin-top:5px; margin-right:3px'>"+ startTime[0] +"<br>"+
+	 					  "<i class='fa fa-play' style=' margin-right:5px'></i>"+startTime[1] +"</div>" +"<br>"+ 
+	 					  "<span style='color:black; font-size:24px;  font-family: Courgette, cursive;'>"+"<div style='text-align:center; margin-top:-23px; '>"+"  "+ "<marquee>" + "<a href='"+pending_contest_data[i].contest_url +"' target='_blank' style='color:black'>" +pending_contest_data[i].contest_name+ "</marquee></div>" +
+	 					  "<span style='color:black; font-size:14px; font-family: Inconsolata, ;'>"+"<div style='text-align:center; margin-top:0px; '>" +"<a href='"+pending_contest_data[i].host_url +"' target='_blank' style='color:black'>" +pending_contest_data[i].host_name +"</a></div>" +"<br>"+
+	 					  "<span style='color:black; font-size:12px; font-family: Roboto, sans-serif;'>"+"  "+"<div style='float:right; margin-top:-20px; margin-right:3px; margin-bottom:3px'>"+ endTime[0] +"<br>"+  
+	 					  "<i class='fa fa-stop' style=' margin-right:5px'></i>"+ endTime[1] +"</div>"+ "<br>"+  
+	 					  "<span style='color:black; font-size:18px; margin-top:5px'>"+"<div style='text-align:left; margin-top:-35px'>"+"  "+ pending_contest_data[i].duration +"</span>";
+
+	 	}
+
+		}else{
+
+
+					if(lengthOfContestname < 30) {
+
+	 		 	newDiv.innerHTML ="<img src='/img/default.jpg' style='border:2px solid black; border-radius: 10px; margin-top:1px; margin-left:1px;height:30%;width30%;margin-bottom:12px;'>"+
+	 						"<span style='color:black; font-size:12px; font-family: Roboto, sans-serif;'>"+"  "+"<div style=' float:right; margin-top:5px; margin-right:3px'>"	+startTime[0] +"<br>"+
+	 					  "<i class='fa fa-play' style=' margin-right:5px;'></i>"+startTime[1] +"</div>" +"<br>"+ 
+	 					  "<span style='color:black; font-size:24px;  font-family: Courgette, cursive;'>"+"<div style='text-align:center; margin-top:5px; '>"+"<a href='"+pending_contest_data[i].contest_url +"' target='_blank' style='color:black'>" +pending_contest_data[i].contest_name+ "</a></div>" +
+	 					  "<span style='color:black; font-size:14px; font-family: Inconsolata, ;'>"+"<div style='text-align:center; margin-top:0px; '>"+"<a href='"+pending_contest_data[i].host_url +"' target='_blank' style='color:black'>" + pending_contest_data[i].host_name +"</a></div>" +"<br>"+
+	 					  "<span style='color:black; font-size:12px; font-family: Roboto, sans-serif;'>"+"  "+"<div style='float:right; margin-top:-20px; margin-right:3px; margin-bottom:3px'>"+ endTime[0] +"<br>"+  
+	 					  "<i class='fa fa-stop' style=' margin-right:5px'></i>"+ endTime[1] +"</div>"+ "<br>"+  
+	 					  "<span style='color:black; font-size:18px; margin-top:5px'>"+"<div style='text-align:left; margin-top:-35px'>"+"  "+ pending_contest_data[i].duration +"</span>";
+
+				
+	 	}else{
+				newDiv.innerHTML ="<img src='/img/default.jpg' style='border:2px solid black; border-radius: 10px; margin-top:1px; margin-left:1px;height:30%;width30%;margin-bottom:12px;'>"+
+	 					  "<span style='color:black; font-size:12px; font-family: Roboto, sans-serif;'>"+"  "+"<div style=' float:right; margin-top:5px; margin-right:3px'>"+ startTime[0] +"<br>"+
+	 					  "<i class='fa fa-play' style=' margin-right:5px'></i>"+startTime[1] +"</div>" +"<br>"+ 
+	 					  "<span style='color:black; font-size:24px;  font-family: Courgette, cursive;'>"+"<div style='text-align:center; margin-top:-23px; '>"+"  "+ "<marquee>" + "<a href='"+pending_contest_data[i].contest_url +"' target='_blank' style='color:black'>" +pending_contest_data[i].contest_name+ "</marquee></div>" +
+	 					  "<span style='color:black; font-size:14px; font-family: Inconsolata, ;'>"+"<div style='text-align:center; margin-top:0px; '>" +"<a href='"+pending_contest_data[i].host_url +"' target='_blank' style='color:black'>" +pending_contest_data[i].host_name +"</a></div>" +"<br>"+
+	 					  "<span style='color:black; font-size:12px; font-family: Roboto, sans-serif;'>"+"  "+"<div style='float:right; margin-top:-20px; margin-right:3px; margin-bottom:3px'>"+ endTime[0] +"<br>"+  
+	 					  "<i class='fa fa-stop' style=' margin-right:5px'></i>"+ endTime[1] +"</div>"+ "<br>"+  
+	 					  "<span style='color:black; font-size:18px; margin-top:5px'>"+"<div style='text-align:left; margin-top:-35px'>"+"  "+ pending_contest_data[i].duration +"</span>";
+
+	 	}
+
+		}
+
+
+
+	 	if ((typeof localStorage["hosts"]) === 'undefined') {
+			document.getElementById("pending-contests").appendChild(newDiv);
+		}
+		else if (hosts.hasOwnProperty(pending_contest_data[i].host_name)) {
+			document.getElementById("pending-contests").appendChild(newDiv);
+		}
+	}
+}
+
+
+
+
+function generateCardsArchive(data) {
+
+	var archived_tabs = document.getElementById("archived-contests");
+	archived_tabs.innerHTML = "";
+	// archived_tabs.innerText = JSON.parse(data)["archived"];
+
+	var archived_contest_data = JSON.parse(data)["archived"];
+	console.log(archived_contest_data);
+	var randomNumber = 0;
+	hosts = JSON.parse(localStorage.getItem('hosts'));
+	
+	for (var i = 0; i < archived_contest_data.length; i++) {
+
+		var newDiv = document.createElement('div');
+		newDiv.style.width = "360px";
+	 	newDiv.style.height = "130px"; 
+	 	newDiv.style.borderStyle = "solid"; 
+	 	newDiv.style.borderWidth = "2px";
+	 	newDiv.style.borderColor = "black";
+	 	newDiv.style.borderRadius = "8px 8px 8px 8px";
+	 	newDiv.style.borderColor = "black";
+	 	newDiv.style.marginBottom = "15px";
+	 
+
+	 	if(randomNumber<9){
+	 		randomNumber = randomNumber+1;
+		}else{
+			randomNumber = 1;
+		}
+
+
+	 	if(randomNumber==1){
+			newDiv.style.background = "#66FF33"; 
+	 	}else if(randomNumber==4){
+			newDiv.style.background = "#FFFF4D"; 
+	 	}else if(randomNumber==6){
+			newDiv.style.background = "#FF8533"; 
+	 	}else if(randomNumber==2){
+			newDiv.style.background = "#FF85AD"; 
+	 	}else if(randomNumber==8){
+			newDiv.style.background = "#DB94FF"; 
+	 	}else if(randomNumber==3){
+			newDiv.style.background = "#70B8FF"; 
+	 	}else if(randomNumber==5){
+			newDiv.style.background = "#82FFFF"; 
+	 	}else if(randomNumber==7){
+			newDiv.style.background = "#CCFF33"; 
+	 	}else if(randomNumber==9){
+			newDiv.style.background = "#47DAB5"; 
+	 	}
+
+		var startTime = toTimeZone(archived_contest_data[i].start).split(",");
+		var endTime = toTimeZone(archived_contest_data[i].end).split(",");
+
+		var lengthOfContestname = archived_contest_data[i].contest_name.length;
+		
+		if(imageExists("/img/"+archived_contest_data[i].host_name+".png")){
+			if(lengthOfContestname < 30) {
+				console.log("hello" + i);
+	 		 	newDiv.innerHTML ="<img src='/img/"+ archived_contest_data[i].host_name  +".png' style='border:2px solid black; border-radius: 10px; margin-top:1px; margin-left:1px;height:30%;width30%;margin-bottom:12px;'>"+
+	 						"<span style='color:black; font-size:12px; font-family: Roboto, sans-serif;'>"+"  "+"<div style=' float:right; margin-top:5px; margin-right:3px'>"	+startTime[0] +"<br>"+
+	 					  "<i class='fa fa-play' style=' margin-right:5px;'></i>"+startTime[1] +"</div>" +"<br>"+ 
+	 					  "<span style='color:black; font-size:24px;  font-family: Courgette, cursive;'>"+"<div style='text-align:center; margin-top:5px; '>"+"<a href='"+archived_contest_data[i].contest_url +"' target='_blank' style='color:black'>" +archived_contest_data[i].contest_name+ "</a></div>" +
+	 					  "<span style='color:black; font-size:14px; font-family: Inconsolata, ;'>"+"<div style='text-align:center; margin-top:0px; '>"+"<a href='"+archived_contest_data[i].host_url +"' target='_blank' style='color:black'>" + archived_contest_data[i].host_name +"</a></div>" +"<br>"+
+	 					  "<span style='color:black; font-size:12px; font-family: Roboto, sans-serif;'>"+"  "+"<div style='float:right; margin-top:-20px; margin-right:3px; margin-bottom:3px'>"+ endTime[0] +"<br>"+  
+	 					  "<i class='fa fa-stop' style=' margin-right:5px'></i>"+ endTime[1] +"</div>"+ "<br>"+  
+	 					  "<span style='color:black; font-size:18px; margin-top:5px'>"+"<div style='text-align:left; margin-top:-35px'>"+"  "+ archived_contest_data[i].duration +"</span>";
+
+				
+		 	} else {
+					newDiv.innerHTML ="<img src='/img/"+ archived_contest_data[i].host_name +".png' style='border:2px solid black; border-radius: 10px; margin-top:1px; margin-left:1px;height:30%;width30%;margin-bottom:12px;'>"+
+		 					  "<span style='color:black; font-size:12px; font-family: Roboto, sans-serif;'>"+"  "+"<div style=' float:right; margin-top:5px; margin-right:3px'>"+ startTime[0] +"<br>"+
+		 					  "<i class='fa fa-play' style=' margin-right:5px'></i>"+startTime[1] +"</div>" +"<br>"+ 
+		 					  "<span style='color:black; font-size:24px;  font-family: Courgette, cursive;'>"+"<div style='text-align:center; margin-top:-23px; '>"+"  "+ "<marquee>" + "<a href='"+archived_contest_data[i].contest_url +"' target='_blank' style='color:black'>" +archived_contest_data[i].contest_name+ "</marquee></div>" +
+		 					  "<span style='color:black; font-size:14px; font-family: Inconsolata, ;'>"+"<div style='text-align:center; margin-top:0px; '>" +"<a href='"+archived_contest_data[i].host_url +"' target='_blank' style='color:black'>" +archived_contest_data[i].host_name +"</a></div>" +"<br>"+
+		 					  "<span style='color:black; font-size:12px; font-family: Roboto, sans-serif;'>"+"  "+"<div style='float:right; margin-top:-20px; margin-right:3px; margin-bottom:3px'>"+ endTime[0] +"<br>"+  
+		 					  "<i class='fa fa-stop' style=' margin-right:5px'></i>"+ endTime[1] +"</div>"+ "<br>"+  
+		 					  "<span style='color:black; font-size:18px; margin-top:5px'>"+"<div style='text-align:left; margin-top:-35px'>"+"  "+ archived_contest_data[i].duration +"</span>";
+
+		 	}
+
+		} else {
+
+			console.log("hello" + i);
+			if(lengthOfContestname < 30) {
+
+		 		 	newDiv.innerHTML ="<img src='/img/default.jpg' style='border:2px solid black; border-radius: 10px; margin-top:1px; margin-left:1px;height:30%;width30%;margin-bottom:12px;'>"+
+		 						"<span style='color:black; font-size:12px; font-family: Roboto, sans-serif;'>"+"  "+"<div style=' float:right; margin-top:5px; margin-right:3px'>"	+startTime[0] +"<br>"+
+		 					  "<i class='fa fa-play' style=' margin-right:5px;'></i>"+startTime[1] +"</div>" +"<br>"+ 
+		 					  "<span style='color:black; font-size:24px;  font-family: Courgette, cursive;'>"+"<div style='text-align:center; margin-top:5px; '>"+"<a href='"+archived_contest_data[i].contest_url +"' target='_blank' style='color:black'>" +archived_contest_data[i].contest_name+ "</a></div>" +
+		 					  "<span style='color:black; font-size:14px; font-family: Inconsolata, ;'>"+"<div style='text-align:center; margin-top:0px; '>"+"<a href='"+archived_contest_data[i].host_url +"' target='_blank' style='color:black'>" + archived_contest_data[i].host_name +"</a></div>" +"<br>"+
+		 					  "<span style='color:black; font-size:12px; font-family: Roboto, sans-serif;'>"+"  "+"<div style='float:right; margin-top:-20px; margin-right:3px; margin-bottom:3px'>"+ endTime[0] +"<br>"+  
+		 					  "<i class='fa fa-stop' style=' margin-right:5px'></i>"+ endTime[1] +"</div>"+ "<br>"+  
+		 					  "<span style='color:black; font-size:18px; margin-top:5px'>"+"<div style='text-align:left; margin-top:-35px'>"+"  "+ archived_contest_data[i].duration +"</span>";
+
+					
+		 	}else{
+					newDiv.innerHTML ="<img src='/img/default.jpg' style='border:2px solid black; border-radius: 10px; margin-top:1px; margin-left:1px;height:30%;width30%;margin-bottom:12px;'>"+
+		 					  "<span style='color:black; font-size:12px; font-family: Roboto, sans-serif;'>"+"  "+"<div style=' float:right; margin-top:5px; margin-right:3px'>"+ startTime[0] +"<br>"+
+		 					  "<i class='fa fa-play' style=' margin-right:5px'></i>"+startTime[1] +"</div>" +"<br>"+ 
+		 					  "<span style='color:black; font-size:24px;  font-family: Courgette, cursive;'>"+"<div style='text-align:center; margin-top:-23px; '>"+"  "+ "<marquee>" + "<a href='"+archived_contest_data[i].contest_url +"' target='_blank' style='color:black'>" +archived_contest_data[i].contest_name+ "</marquee></div>" +
+		 					  "<span style='color:black; font-size:14px; font-family: Inconsolata, ;'>"+"<div style='text-align:center; margin-top:0px; '>" +"<a href='"+archived_contest_data[i].host_url +"' target='_blank' style='color:black'>" +archived_contest_data[i].host_name +"</a></div>" +"<br>"+
+		 					  "<span style='color:black; font-size:12px; font-family: Roboto, sans-serif;'>"+"  "+"<div style='float:right; margin-top:-20px; margin-right:3px; margin-bottom:3px'>"+ endTime[0] +"<br>"+  
+		 					  "<i class='fa fa-stop' style=' margin-right:5px'></i>"+ endTime[1] +"</div>"+ "<br>"+  
+		 					  "<span style='color:black; font-size:18px; margin-top:5px'>"+"<div style='text-align:left; margin-top:-35px'>"+"  "+ archived_contest_data[i].duration +"</span>";
+
+		 	}
+		}
+
+	 	if ((typeof localStorage["hosts"]) === 'undefined') {
+			document.getElementById("archived-contests").appendChild(newDiv);
+			console.log("hello" + i);
+		}
+		else if (hosts.hasOwnProperty(archived_contest_data[i].host_name)) {
+			document.getElementById("archived-contests").appendChild(newDiv);
+			console.log("hello" + i);
+		}
+	}
+}
+
 function imageExists(image_url){
-
-    var http = new XMLHttpRequest();
-
-    http.open('GET', image_url, false);
-    http.send();
-
-    return http.status != 404;
+	try {
+	    var http = new XMLHttpRequest();
+	    http.open('GET', image_url, false);
+	    http.send();
+	    return http.status != 404;
+	} catch (err) {
+		return false;
+	}
 }
 
 // select all the options from drop down which are already in the local storage
