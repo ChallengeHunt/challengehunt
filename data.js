76182e8f9aa625fc48f1c4d3fb5897e8e1755eef
chirrag <chirragnangia@gmail.com>
2015-03-03 17:28:19 +0530

    Added color on hover on cards

--- data.js
+++ data.js
@@ -156,7 +156,7 @@ function generateCards(data) {
 			contestNameDiv.style.cssText =  "font-size:24px;  font-family: Courgette, cursive; text-align:center; margin-top:-4px; "	
 			// contestNameDiv.innerHTML = "<marquee behavior='scroll' id='link'><a href='"+active_contest_data[i].contest_url +"' target='_blank'>" +active_contest_data[i].contest_name+ "</a></marquee>";
 						// contestNameDiv.className = "marquee";
-			contestNameDiv.innerHTML = "<marquee direction='right' behavior='alternate' width='200'><a href='"+active_contest_data[i].contest_url +"' target='_blank'>" +active_contest_data[i].contest_name+ "</a></marquee>";
+			contestNameDiv.innerHTML = "<marquee direction='left' behavior='scroll'><a href='"+active_contest_data[i].contest_url +"' target='_blank'>" +active_contest_data[i].contest_name+ "</a></marquee>";
 
 			newDiv.appendChild(contestNameDiv);			
 
