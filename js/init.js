d668554b3e32abc2830d683368cb578cb3c072ad
Archit Verma <architv07@gmail.com>
2016-02-08 02:51:55 +0530

    added intro for challenge hunt

--- js/init.js
+++ js/init.js
@@ -1,2 +1,12 @@
 $.material.init();
 $.material.ripples();
+
+if (!localStorage.init) {
+	introJs().setOption('showProgress', true).start();
+	introJs().addHints();
+	localStorage.init = 1;
+}
+
+$("#help").click(function(){
+	introJs().setOption('showProgress', true).start();
+});
